68aef0b8094d9b7bfa06044ee8e3fec3f70ef67b
Abdullah yasin <abdullahyasinforwork@gmail.com>
2021-09-19 22:45:18 +0200

    build navigation menu

--- js/app.js
+++ js/app.js
@@ -3,3 +3,25 @@
 const listOfSections = document.querySelectorAll('section');
 const ulNav = document.getElementById('navbar__list');
 const listOfli = document.querySelectorAll('li');
+
+
+
+//build naviagation menu
+
+const ulFragment = document.createDocumentFragment();
+
+listOfSections.forEach(function(elem){
+let liElement = document.createElement('li');
+liElement.classList.add('menuLink');
+liElement.textContent = elem.dataset.nav;
+// Scroll to section on link click
+liElement.addEventListener('click',function(){
+    elem.scrollIntoView({
+        behavior : "smooth"
+    })
+})
+ulFragment.appendChild(liElement);
+});
+
+ulNav.appendChild(ulFragment);
+
