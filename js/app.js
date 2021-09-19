200f653278a68e810b89ab348a08e38ae9c31cd7
Abdullah yasin <abdullahyasinforwork@gmail.com>
2021-09-19 22:38:37 +0200

    define global variables

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//global variables
+
+const listOfSections = document.querySelectorAll('section');
+const ulNav = document.getElementById('navbar__list');
+const listOfli = document.querySelectorAll('li');
